49917b5aa2bc2f3ec580c6e8d92bd8ef15e31d51
sundara_rajan <Sundara_Rajan@epam.com>
2024-11-07 21:54:41 +0000

    CODE FOR COSMOSDB INTEGRATION

--- OrdersCosmos/Program.cs
+++ OrdersCosmos/Program.cs
@@ -1,7 +1,21 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.Azure.Cosmos;
+using Product_API.Services;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+string? url = builder.Configuration.GetSection("AzureCosmoDBSettings").GetValue<string>("URL");
+string? primaryKey = builder.Configuration.GetSection("AzureCosmoDBSettings").GetValue<string>("PrimaryKey");
+string? dbName = builder.Configuration.GetSection("AzureCosmoDBSettings").GetValue<string>("DatabaseName");
+string? containerName = builder.Configuration.GetSection("AzureCosmoDBSettings").GetValue<string>("ContainerName");
+
+builder.Services.AddSingleton<IProductService>(options =>
+{
+    var cosmosClient = new CosmosClient(url, primaryKey);
+    return new ProductService(cosmosClient, dbName, containerName);
+
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
---
+++ OrdersCosmos/Services/IProductInterface.cs
@@ -0,0 +1,13 @@
+﻿using Product_API.Models;
+namespace Product_API.Services
+{
+
+    public interface IProductService
+    {
+
+        Task<List<ProductModel>> Get(string query);
+        Task<ProductModel> Add(ProductModel task);
+        Task<ProductModel> Update(ProductModel task);
+        Task Delete(string id, string partition);
+    }
+}
